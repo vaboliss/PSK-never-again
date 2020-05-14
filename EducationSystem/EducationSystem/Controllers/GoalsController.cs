5a057b6fff62b11981e9a97b25f294c3b8ba58e3
kajusn <kajusn@gmail.com>
2020-05-14 15:22:10 +0300

    Generated Goals controller, views, created goal service

---
+++ EducationSystem/EducationSystem/Controllers/GoalsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EducationSystem.Data;
+using EducationSystem.Models;
+
+namespace EducationSystem.Controllers
+{
+    public class GoalsController : Controller
+    {
+        private readonly EducationSystemDbContext _context;
+
+        public GoalsController(EducationSystemDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Goals
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Goals.ToListAsync());
+        }
+
+        // GET: Goals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var goal = await _context.Goals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return View(goal);
+        }
+
+        // GET: Goals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Goals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] Goal goal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(goal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(goal);
+        }
+
+        // GET: Goals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+            return View(goal);
+        }
+
+        // POST: Goals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] Goal goal)
+        {
+            if (id != goal.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(goal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GoalExists(goal.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(goal);
+        }
+
+        // GET: Goals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var goal = await _context.Goals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return View(goal);
+        }
+
+        // POST: Goals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+            _context.Goals.Remove(goal);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GoalExists(int id)
+        {
+            return _context.Goals.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EducationSystem/EducationSystem/Provider/GoalService.cs
@@ -0,0 +1,18 @@
+﻿using EducationSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationSystem.Provider
+{
+    public class GoalService
+    {
+        private readonly EducationSystemDbContext _edu;
+
+        public GoalService(EducationSystemDbContext edu)
+        {
+            _edu = edu;
+        }
+    }
+}
