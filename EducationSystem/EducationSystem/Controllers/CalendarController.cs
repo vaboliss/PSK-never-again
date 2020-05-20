9b28063e60bbdf356784f1a0dac2629ca95192cd
kajusn <kajusn@gmail.com>
2020-05-20 18:57:15 +0300

    Changed learning day model to include date, added GetLearningDays functionality to display on the calendar

--- EducationSystem/EducationSystem/Controllers/CalendarController.cs
+++ EducationSystem/EducationSystem/Controllers/CalendarController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EducationSystem.Data;
+using EducationSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EducationSystem.Controllers
 {
     public class CalendarController : Controller
     {
+        private readonly EducationSystemDbContext _context;
+
+        public CalendarController(EducationSystemDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetLearningDays()
+        {
+            List<LearningDay> learningDays = _context.LearningDays.ToList(); // TO-DO: Should GET the learning days for the specific user and/or his subordinates
+
+            return Json(learningDays);
         }
     }
 }
--- EducationSystem/EducationSystem/Models/LearningDay.cs
+++ EducationSystem/EducationSystem/Models/LearningDay.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace EducationSystem.Models
 {
     public class LearningDay
     {
         public int Id { get; set; }
-        public virtual ICollection<Topic> Topics { get; set; }
+        public DateTime Date { get; set; }
+        public int TopicId { get; set; }
+        public virtual Topic Topic { get; set; }
         public string Comment { get; set; }
     }
 }
