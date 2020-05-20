e96b4db5dd24adc17e4faa7f94a2b20a264b8def
kajusn <kajusn@gmail.com>
2020-05-20 19:39:12 +0300

    Removed unused dependencies

--- EducationSystem/EducationSystem/Controllers/CalendarController.cs
+++ EducationSystem/EducationSystem/Controllers/CalendarController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.Xml;
 
 namespace EducationSystem.Controllers
 {
