234307e760720948d48bfeed40954c67d8c0e1ff
kajusn <kajusn@gmail.com>
2020-05-26 19:09:55 +0300

    Removed unecessary method

--- EducationSystem/EducationSystem/Provider/LearningDayService.cs
+++ EducationSystem/EducationSystem/Provider/LearningDayService.cs
@@ -2,7 +2,6 @@
 using EducationSystem.Interfaces;
 using EducationSystem.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -30,21 +29,5 @@ namespace EducationSystem.Provider
             List<LearningDay> learningDays = _edu.LearningDays.Where(ld => ld.WorkerId == worker.Id).Include(ld => ld.Topic).ToList();
             return learningDays;
         }
-        /*
-        public LearningDay CreateLearningDay(int id, int topicId, DateTime date, string comment)
-        {
-            Topic topic = _edu.Find<Topic>(topicId);
-            if (topic == null)
-            {
-                return ;
-            }
-            LearningDay learningDay = new LearningDay();
-            learningDay.Topic = topic;
-            learningDay.TopicId = topic.Id;
-            learningDay.Date = date;
-            _edu.Add(learningDay);
-            _edu.SaveChanges();
-            return true;
-        }*/
     }
 }
